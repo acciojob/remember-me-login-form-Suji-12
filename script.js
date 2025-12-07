7dcb55bbaafbc901768857417b41bfb80a4f856a
Suji-12 <23csec23sujithas@gmail.com>
2025-12-07 10:19:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const checkbox = document.getElementById("checkbox");
+const submitBtn = document.getElementById("submit");
+const existingBtn = document.getElementById("existing");
+
+function loadSavedCredentials() {
+  const savedUser = localStorage.getItem("username");
+  const savedPass = localStorage.getItem("password");
+  if (savedUser && savedPass) {
+    existingBtn.style.display = "block";
+  }
+}
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  alert(`Logged in as ${username}`);
+
+  if (checkbox.checked) {
+
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+
+    existingBtn.style.display = "block"; 
+  } else {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+
+    existingBtn.style.display = "none";
+  }
+});
+
+existingBtn.addEventListener("click", function () {
+  const savedUser = localStorage.getItem("username");
+  alert(`Logged in as ${savedUser}`);
+});
+
+loadSavedCredentials();
+
